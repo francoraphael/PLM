89aae9db935ed8c67d4eb73f60894e41f87b8a87
Raphael Franco <raphael.franco@eurekalabs.com.br>
2019-11-15 18:11:30 -0300

    Modifica limpeza da pilha

--- Elevador/elevador.s
+++ Elevador/elevador.s
@@ -1,25 +1,24 @@
 .section .data
 
-divide_tela: .asciz "===================================================\n"
-titulo: .asciz "Simulador de elevador\n"
-subindo: .asciz "Subindo..."
-descendo: .asciz "Descendo..."
+divide_tela:  .asciz "===================================================\n"
+titulo:       .asciz "Simulador de elevador\n"
+subindo:      .asciz "Subindo..."
+descendo:     .asciz "Descendo..."
 
 .section .text
 
 .globl main
 main:
   pushl $divide_tela
-  call printf
-  popl divide_tela
+  call  printf
 
   pushl $titulo
-  call printf
-  popl titulo
+  call  printf
 
   pushl $divide_tela
-  call printf
-  popl divide_tela
+  call  printf
 
+  addl  $16,  %esp
+  
   pushl $0
-  call exit
+  call  exit
