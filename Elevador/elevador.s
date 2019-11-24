57106c15699aaa19860b8001c00f4b0d67bec284
Raphael Franco <raphael.franco@eurekalabs.com.br>
2019-11-24 21:42:52 +0000

    Parcial sorteio de chamadas internas

--- Elevador/elevador.s
+++ Elevador/elevador.s
@@ -10,12 +10,16 @@ pessoas_saindo:         .asciz "\n%d pessoa(s) saindo do elevador"
 pessoas_entrando:       .asciz "\n%d pessoa(s) entrando no elevador"
 pessoas_no_elevador:    .asciz "\n%d pessoa(s) dentro do elevador"
 formato_int:            .asciz "%d"
+chamadas_internas:      .asciz "\nChamada interna %d ida ao andar: %d"
 string_teste:           .asciz "\nTeste leitura valores: %d e %d\n"
 qtd_andares:            .int 0
 qtd_pessoas_elevador:   .int 0
 probabilidade_evento:   .int 0
 direcao:                .int 0 # SUBINDO (0) ou DESCENDO (1)
 andar_atual:            .int 0
+qtd_pessoas_entrando:   .int 0
+contador:               .int 0
+tempo:                  .int 4
 string_debug:           .asciz "\n Teste: %d"
 string_debug_2:         .asciz "\n Teste: %X"
 
@@ -32,15 +36,26 @@ verifica_lista_externa:
   mull %ebx # eax = eax * ebx || isso eh pra calcular o offset em bytes a ser andado na lista
   movl $lista_externa, %edi # move o inicio da lista para edi
   addl %eax, %edi # caminha na lista externa para o andar atual
-  cmpl $0, (%edi) # verifica se existe alguem que deseja entrar naquele andar
+  pushl (%edi)
+  popl qtd_pessoas_entrando
+  cmpl $0, qtd_pessoas_entrando # verifica se existe alguem que deseja entrar naquele andar
   jle retorno # caso nao exista, sai da funcao
   movl qtd_pessoas_elevador, %ebx # move qtd_pessoas_elevador para ebx
-  addl (%edi), %ebx # qtd_pessoas_elevador = qtd_pessoas_elevador + qtd_pessoas_entrando
+  addl qtd_pessoas_entrando, %ebx # qtd_pessoas_elevador = qtd_pessoas_elevador + qtd_pessoas_entrando
   movl %ebx, qtd_pessoas_elevador # atualiza qtd_pessoas_elevador
-  pushl (%edi) # poem qtd_pessoa_entrando na pilha
+  pushl qtd_pessoas_entrando # poem qtd_pessoa_entrando na pilha
   pushl $pessoas_entrando # poem string para exibir qtd pessoas saindo na pilha
   call printf # chamada externa printf
   addl $8, %esp # limpa pilha
+  pushl $tempo
+  call time
+  pushl tempo
+  call srand
+  addl $8, %esp
+  movl qtd_pessoas_entrando, %ecx
+loop_insere_lista_interna:
+
+  loop loop_insere_lista_interna
   ret # retorna
 
 verifica_lista_interna:
@@ -59,8 +74,9 @@ verifica_lista_interna:
   call printf # chamada externa printf
   addl $8, %esp # limpa pilha
   ret # retorna
-retorno:
-  ret
+
+retorno: # metodo dummy para retornar
+  ret # retorna
 
 .globl main
 main:
@@ -106,14 +122,16 @@ main:
     call  printf # chamada externa ao printf
     addl $4, %esp # limpa pilha
 
+    pushl %ecx
     call verifica_lista_interna # verifica lista interna no andar atual, imprime e remove
     call verifica_lista_externa # verifica lista externa no andar atual, imprime e adiciona
+    popl %ecx
 
-    pushl qtd_pessoas # insere quantidade de pessoas na pilha
+    pushl qtd_pessoas_elevador # insere quantidade de pessoas na pilha
     pushl $pessoas_no_elevador # insere string para exibir qtd de pessoas na pilha
     call printf # chamada externa ao printf
 
-    addl $8, %esp limpa pilha
+    addl $8, %esp #limpa pilha
    
     # INFOS SOBRE O ESTADO DO ELEVADOR DEVEM SER COLOCADAS AQUI
 
