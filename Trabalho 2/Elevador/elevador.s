9f2f44ebff8df4f8282e57e9bb08b591574847da
Ubuntu <ubuntu@ip-172-31-86-247.ec2.internal>
2020-01-23 00:09:18 +0000

    Metodo adicionar pessoa

--- Trabalho 2/Elevador/elevador.s
+++ Trabalho 2/Elevador/elevador.s
@@ -17,6 +17,7 @@ string_chamadas_externas:                  .asciz "%d chamada(s) externa(s) fora
 string_limite_andares:                     .asciz "Erro: quantidade de andares deve ser entre 0 e 50\n"
 string_limite_probabilidade:               .asciz "Erro: probabilidade deve ser entre 0 e 100\n"
 formato_int:                               .asciz "%d"
+teste_inicializacao:                       .asciz "Pos %d -> %d\n"
 qtd_andares:                               .int 0
 qtd_pessoas_elevador1:                     .int 0
 qtd_pessoas_elevador2:                     .int 0
@@ -37,11 +38,62 @@ formach:                      .asciz "%c"
 
 .section .bss
 
+# struct pessoa
+# andar_alvo # 4 Bytes
+# idade # 4 Bytes
+# peso # 4 Bytes
+# proximo # 4 Bytes
+
 .lcomm lista_interna_elevador1, 200 # lista de pronteiros (4 Bytes cada posicao)
 .lcomm lista_interna_elevador2, 200 # lista de pronteiros (4 Bytes cada posicao)
 .lcomm lista_externa, 200 # lista de pronteiros (4 Bytes cada posicao)
 
 .section .text
+
+retorna:
+  ret
+
+# Registradores utilizados: eax, ecx
+# Parametros (lista)
+inicializa_lista:
+  pushl %ebp
+  movl %esp, %ebp
+  movl 8(%ebp), %eax
+  movl $50, %ecx
+  loop_inicializacao_lista:
+    movl $-1, (%eax)
+    addl $4, %eax
+    loop loop_inicializacao_lista
+
+  popl %ebp
+  ret
+              
+# Parametros (lista, ponteiro_pessoa)
+adiciona_pessoa_lista:
+  pushl %ebp
+  movl %esp, %ebp
+  movl 12(%ebp), %edi # recupera ponteiro_pessoa
+  movl (%edi), %eax # move andar_alvo para eax
+  movl $4, %edx # move tamanho do ponteiro para edx
+  mull %edx # calcula quanto tem de andar na lista
+  movl 8(%ebp), %edx # recupera lista
+  addl %eax, %edx # desloca a lista em edx
+  cmpl $-1, (%edx) # verifica se posicao andar_atual esta vazia na lista
+  je insere_proximo
+  movl (%edx), %edx
+  addl $12, %edx # anda na pessoa para campo ponteiro_proximo
+  loop_percorre_lista:
+    cmpl $-1, (%edx) # verifica se é um ponteiro_proximo ou fim da lista (-1)
+    je insere_proximo
+    movl (%edx), %edx # move ponteiro_proximo (nova pessoa), para edi
+    addl $12, %edx # desloca na nova pessoa para o campo ponteiro_proximo
+    jmp loop_percorre_lista
+
+  insere_proximo:
+    movl %edi, (%edx) 
+  popl %ebp
+  ret
+
 .globl main
 main:
   pushl $tempo # variavel que vai receber a chamada do time
@@ -85,17 +137,17 @@ main:
     jl print_erro_limite_probabilidade # fim validação
     jmp fluxo_sem_erro # vai para fluxo onde validações passaram
 
-      print_erro_limite_andares:
-        pushl $string_limite_andares
-        call printf
-        addl $20, %esp # limpa pilha até aquele momento
-        jmp inicio_leituras
+    print_erro_limite_andares:
+      pushl $string_limite_andares
+      call printf
+      addl $20, %esp # limpa pilha até aquele momento
+      jmp inicio_leituras
 
-      print_erro_limite_probabilidade:
-        pushl $string_limite_probabilidade
-        call printf
-        addl $20, %esp # limpa pilha até aquele momento
-        jmp inicio_leituras
+    print_erro_limite_probabilidade:
+      pushl $string_limite_probabilidade
+      call printf
+      addl $20, %esp # limpa pilha até aquele momento
+      jmp inicio_leituras
 
     fluxo_sem_erro:    
       addl $28, %esp # limpa a pilha
@@ -122,20 +174,36 @@ main:
       call scanf # limpa o buffer do teclado
       addl $4, %esp # limpa o buffer do teclado
 
-    loop_infinito: # rotulo para loop infinito do elevador
-      pushl $divide_tela # insere string divide_tela na pilha
-      call  printf # chamada externa ao printf
-      addl $4, %esp # limpa pilha
+  loop_infinito: # rotulo para loop infinito do elevador
+    pushl $divide_tela # insere string divide_tela na pilha
+    call  printf # chamada externa ao printf
+    addl $4, %esp # limpa pilha
 
-      # DESENVOLVER AQUI
+    pushl $lista_interna_elevador1
+    call inicializa_lista
+    addl $4, %esp
 
-      pushl $divide_tela # insere string divide_tela na pilha
-      call  printf # chamada externa ao printf
-      addl $8, %esp # limpa pilha
+    pushl $20
+    call malloc
 
-      call getchar # para ler o resultado antes o elevador continuar. apertar apenas ENTER
+    movl $3, (%eax)
+    pushl %eax
+    pushl $lista_interna_elevador1
+    call adiciona_pessoa_lista
+    addl $8, %esp
 
-      jmp loop_infinito # loop
+    movl $lista_interna_elevador1, %eax
+    addl $12, %eax
+
+    # DESENVOLVER AQUI
+
+    pushl $divide_tela # insere string divide_tela na pilha
+    call  printf # chamada externa ao printf
+    addl $8, %esp # limpa pilha
+
+    call getchar # para ler o resultado antes o elevador continuar. apertar apenas ENTER
+
+    jmp loop_infinito # loop
 
   fim:
     pushl $0
